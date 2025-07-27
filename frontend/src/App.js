91859823bea599697d5ba7232aa8279b48d3048b
Raanan Peretz <47702617+raananp@users.noreply.github.com>
2025-07-27 21:51:30 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,23 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import About from './pages/About';
+import Bikes from './pages/Bikes';
+import Booking from './pages/Booking';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            Bike Rental
-          </Typography>
-          <Button color="inherit" component={Link} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/about">
-            About
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <Container style={{ marginTop: '2rem' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Container>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bikes" element={<Bikes />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
-
